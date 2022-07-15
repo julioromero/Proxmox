294a24de91ecd571e50c36a340559696aa32f8a6
julioromero <luloromero19@gmail.com>
2022-07-15 12:55:54 -0400

    Update haos-vm-v3.sh
    change disk size to 16gb

--- vm/haos-vm-v3.sh
+++ vm/haos-vm-v3.sh
@@ -334,7 +334,7 @@ pvesm alloc $STORAGE $VMID $DISK0 4M 1>&/dev/null
 qm importdisk $VMID ${FILE%.*} $STORAGE ${DISK_IMPORT:-} 1>&/dev/null
 qm set $VMID \
   -efidisk0 ${DISK0_REF},efitype=4m,size=4M \
-  -scsi0 ${DISK1_REF},size=32G >/dev/null
+  -scsi0 ${DISK1_REF},size=16G >/dev/null
 qm set $VMID \
   -boot order=scsi0 >/dev/null
 
